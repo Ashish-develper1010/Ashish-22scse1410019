6371d87611605ba1e045251847eb60e495d79ece
Ashish-develper1010 <ac6100861@gmail.com>
2024-12-27 20:02:17 +0530

    problem2.java

---
+++ ashish-22scse1410019/problem2.java
@@ -0,0 +1,11 @@
+2. WAP to take input 1 number from the user and print its square and cube.
+  public class questions {
+    public static void main(String[] args)
+  {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number ");
+        int n = sc.nextInt();
+        System.out.println("Square of "+n+" is : "+n*n);
+        System.out.println("Cube of "+n+" is : "+n*n*n);
+    }
+}
