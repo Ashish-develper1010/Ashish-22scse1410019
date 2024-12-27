1b0e206aa6ea47ae1e04e2942d5f8cf962f11107
Ashish-develper1010 <ac6100861@gmail.com>
2024-12-27 20:14:33 +0530

    Create problem3.java

---
+++ ashish-22scse1410019/problem3.java
@@ -0,0 +1,21 @@
+3. WAP to calculate and print area of circle and circumference of circle.
+  public class questions {
+    public static int area(int rds)
+    {
+        int area = (int) (3.14*rds*rds);
+        return area;
+    }
+    public static int circumference(int rds)
+    {
+        int crcmference = (int)(2*3.14*rds);
+        return crcmference;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius of circle ");
+        int radius = sc.nextInt();
+        System.out.println("Area of the ercle is : "+area(radius));
+        System.out.println("Circumference of the circle is : "+circumference(radius));
+    }
+}
