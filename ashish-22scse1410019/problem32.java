c3c8ffefb336efd58651fc6b8de80432da91367d
Ashish-develper1010 <ac6100861@gmail.com>
2024-12-29 06:27:19 +0530

    Create problem32.java

---
+++ ashish-22scse1410019/problem32.java
@@ -0,0 +1,17 @@
+32. WAP to print the reverse of a number given by user.
+
+import java.util.Scanner;
+
+public class questions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        String a = sc.nextLine();
+        int n = a.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = n-1; i >= 0; i--) {
+            sb.append(a.charAt(i));
+        }
+        System.out.println("The reversed string is : " + sb.toString());
+    }
+}
